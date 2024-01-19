9757def1584a514d8cb7bcc88f99f6dfe9aae45b
Wasim Raja <wasim7raja10@gmail.com>
2024-01-19 20:18:18 +0530

    added font

--- components/layout.js
+++ components/layout.js
@@ -1,16 +1,30 @@
-import Alert from '../components/alert'
-import Footer from '../components/footer'
-import Meta from '../components/meta'
+import { Roboto as FontSans } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import Alert from "../components/alert";
+import Footer from "../components/footer";
+import Meta from "../components/meta";
+
+export const fontSans = FontSans({
+	subsets: ["latin"],
+	variable: "--font-sans",
+	weight: ["300", "400", "500", "700"],
+});
 
 export default function Layout({ preview, children }) {
-  return (
-    <>
-      <Meta />
-      <div className="min-h-screen">
-        <Alert preview={preview} />
-        <main>{children}</main>
-      </div>
-      <Footer />
-    </>
-  )
+	return (
+		<>
+			<Meta />
+			<div
+				className={cn(
+					"min-h-screen bg-background font-sans antialiased",
+					fontSans.variable
+				)}
+			>
+				<Alert preview={preview} />
+				<main>{children}</main>
+			</div>
+			<Footer />
+		</>
+	);
 }
