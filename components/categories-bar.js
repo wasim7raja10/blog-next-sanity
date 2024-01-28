f042b060204aa6cd63a53c1aa300e7084ef9daad
Wasim Raja <wasim7raja10@gmail.com>
2024-01-28 21:56:01 +0530

    categories bar sticky

--- components/categories-bar.js
+++ components/categories-bar.js
@@ -2,17 +2,19 @@ import { ToggleGroup, ToggleGroupItem } from "./ui/toggle-group";
 
 export default function CategoriesBar({ allCategories }) {
 	return (
-		<div className="max-w-max mx-auto py-4 sm:py-10">
-			<ToggleGroup
-				defaultValue="explore"
-				className="justify-start gap-6 overflow-auto"
-				type="single"
-			>
-				<ToggleGroupItem value="explore">Explore</ToggleGroupItem>
-				{allCategories.map((it) => (
-					<ToggleGroupItem value={it.slug}>{it.name}</ToggleGroupItem>
-				))}
-			</ToggleGroup>
+		<div className="py-4 sm:py-10 sticky top-0 bg-background">
+			<div className="max-w-max mx-auto">
+				<ToggleGroup
+					defaultValue="explore"
+					className="justify-start gap-6 overflow-auto"
+					type="single"
+				>
+					<ToggleGroupItem value="explore">Explore</ToggleGroupItem>
+					{allCategories.map((it) => (
+						<ToggleGroupItem value={it.slug}>{it.name}</ToggleGroupItem>
+					))}
+				</ToggleGroup>
+			</div>
 		</div>
 	);
 }
