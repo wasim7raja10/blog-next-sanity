6bf47bac27d6cff4b6883d96dd20e5a66e6d865b
Wasim Raja <wasim7raja10@gmail.com>
2024-02-10 14:05:29 +0530

    fixed image in the post body

--- components/post-body.js
+++ components/post-body.js
@@ -1,9 +1,32 @@
 import { PortableText } from "@portabletext/react";
+import { getImageDimensions } from "@sanity/asset-utils";
+import { urlForImage } from "@/lib/sanity";
 
 export default function PostBody({ content }) {
 	return (
 		<div className="prose prose-lg md:prose-xl prose-p:leading-8">
-			<PortableText value={content} />
+			<PortableText value={content} components={components} />
 		</div>
 	);
 }
+
+const ImageComponent = ({ value, isInline }) => {
+	const { width, height } = getImageDimensions(value);
+	return (
+		<img
+			src={urlForImage(value)}
+			alt={value.alt || " "}
+			loading="lazy"
+			style={{
+				display: isInline ? "inline-block" : "block",
+				aspectRatio: width / height,
+			}}
+		/>
+	);
+};
+
+const components = {
+	types: {
+		image: ImageComponent,
+	},
+};
