7ee41ee78e57cc2854878e56249822eb1a4c3b33
Wasim Raja <wasim7raja10@gmail.com>
2024-02-10 13:21:56 +0530

    class to className

--- components/footer.js
+++ components/footer.js
@@ -5,9 +5,9 @@ export default function Footer() {
 	return (
 		<footer className="bg-accent-1 border-t border-accent-2">
 			<Container>
-				<div class="flex flex-wrap items-center md:justify-between justify-center">
-					<div class="w-full md:w-4/12 px-4 mx-auto text-center">
-						<div class="text-sm text-blueGray-500 font-semibold py-1">
+				<div className="flex flex-wrap items-center md:justify-between justify-center">
+					<div className="w-full md:w-4/12 px-4 mx-auto text-center">
+						<div className="text-sm text-blueGray-500 font-semibold py-1">
 							Copyright © <span id="get-current-year">{year}</span>
 						</div>
 					</div>
