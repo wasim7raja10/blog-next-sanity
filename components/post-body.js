a5fb2c840407fce726bec52467c2224c6d418deb
Wasim Raja <wasim7raja10@gmail.com>
2024-02-13 17:18:51 +0530

    format

--- components/post-body.js
+++ components/post-body.js
@@ -3,6 +3,7 @@ import { urlForImage } from "@/lib/sanity";
 import { cn } from "@/lib/utils";
 import { PortableText, toPlainText } from "@portabletext/react";
 import getSlug from "speakingurl";
+import components from "@/lib/portable-text";
 
 export default function PostBody({ content }) {
 	return (
@@ -11,55 +12,3 @@ export default function PostBody({ content }) {
 		</div>
 	);
 }
-
-const components = {
-	types: {
-		image: ImageComponent,
-	},
-	block: {
-		h2: ({ children, value }) => (
-			<LinkableHeader level={2} value={value}>
-				{children}
-			</LinkableHeader>
-		),
-		h3: ({ children, value }) => (
-			<LinkableHeader level={3} value={value}>
-				{children}
-			</LinkableHeader>
-		),
-		h4: ({ children, value }) => (
-			<LinkableHeader level={4} value={value}>
-				{children}
-			</LinkableHeader>
-		),
-		h5: ({ children, value }) => (
-			<LinkableHeader level={5} value={value}>
-				{children}
-			</LinkableHeader>
-		),
-		h6: ({ children, value }) => (
-			<LinkableHeader level={6} value={value}>
-				{children}
-			</LinkableHeader>
-		),
-	},
-};
-
-const ImageComponent = ({ value, isInline }) => {
-	const { width, height } = getImageDimensions(value);
-	return (
-		<img
-			src={urlForImage(value)}
-			alt={value.alt || " "}
-			loading="lazy"
-			style={{ aspectRatio: width / height }}
-			className={cn("block mx-auto", { "inline-block": isInline })}
-		/>
-	);
-};
-
-const LinkableHeader = ({ children, value, level }) => {
-	const slug = getSlug(toPlainText(value));
-	const Element = `h${level}`;
-	return <Element id={slug}>{children}</Element>;
-};
