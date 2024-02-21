e17b5bbf431903fff9492d7a42f65ee2aaf6def4
Wasim Raja <wasim7raja10@gmail.com>
2024-02-22 01:26:29 +0530

    done

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Page({ params }) {
-	console.log(params);
-	return (
-		<>
-			<h1>{params.category}</h1>
-		</>
-	);
-}
--- components/categories-bar.js
+++ components/categories-bar.js
@@ -1,39 +1,26 @@
 import Link from "next/link";
 import { ToggleGroup, ToggleGroupItem } from "./ui/toggle-group";
-import { useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
 import { categoriesQuery } from "@/lib/queries";
-import useSWR from "swr";
-import { getClient, overlayDrafts } from "@/lib/sanity.server";
+import { sanityClient } from "@/lib/sanity.server";
 
-export default function CategoriesBar() {
-	const pathName = usePathname().slice(1);
-	const [currentCat, setCurrentCat] = useState(pathName || "explore");
-	const { data, isLoading, error } = useSWR(categoriesQuery, async (query) =>
-		overlayDrafts(await getClient().fetch(query))
-	);
-
-	if (isLoading) return "loading ...";
-
+export default async function CategoriesBar({ currentCategory }) {
+	const data = await sanityClient.fetch(categoriesQuery);
 	return (
 		<div className="py-4 mb-6 sm:py-6 sticky top-0 bg-background z-50 -mx-4 sm:mx-0 px-4 sm:px-0">
 			<div className="max-w-max mx-auto">
 				<ToggleGroup
-					value={currentCat}
+					value={currentCategory}
 					className="justify-start gap-3 sm:gap-4 overflow-auto no-scrollbar"
 					type="single"
 					size="sm"
-					onValueChange={(value) => {
-						if (value) console.log(value);
-					}}
 				>
-					<ToggleGroupItem value="explore">
-						<Link href="/">Explore</Link>
-					</ToggleGroupItem>
+					<Link href="/">
+						<ToggleGroupItem value="explore">Explore</ToggleGroupItem>
+					</Link>
 					{data?.map((it) => (
-						<ToggleGroupItem key={it.slug} value={it.slug}>
-							<Link href={it.slug}>{it.name}</Link>
-						</ToggleGroupItem>
+						<Link key={it.slug} href={it.slug}>
+							<ToggleGroupItem value={it.slug}>{it.name}</ToggleGroupItem>
+						</Link>
 					))}
 				</ToggleGroup>
 			</div>
