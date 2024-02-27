4b6ad56b1aeed1d805beea727100447e52816768
Wasim Raja <wasim7raja10@gmail.com>
2024-02-27 18:07:59 +0530

    change confirm route path

--- app/api/auth/confirm/route.js
+++ app/api/auth/confirm/route.js
@@ -1,4 +1,5 @@
 import { createClient } from "@/lib/supabase/server";
+import { NextResponse } from "next/server";
 
 export async function GET(request) {
 	const { searchParams } = new URL(request.url);
