4959c5b5b0225a386583b08684a8d2b952b7d25b
Wasim Raja <wasim7raja10@gmail.com>
2024-02-29 15:09:01 +0530

    logout done

--- components/logout-button.js
+++ components/logout-button.js
@@ -1,11 +1,24 @@
+"use client";
+
+import { logout } from "@/lib/auth/actions";
 import { Button } from "./ui/button";
+import { Loader2 } from "lucide-react";
+import { useFormStatus } from "react-dom";
 
 export default function LogoutButton() {
 	return (
-		<form action={"/api/auth/logout"} method="post">
-			<Button className="bg-Dialog hover:bg-background" variant="outline">
-				Sign out
-			</Button>
+		<form action={logout}>
+			<FormButton />
 		</form>
+	);
+}
+
+function FormButton() {
+	const { pending } = useFormStatus();
+	return (
+		<Button className="bg-Dialog hover:bg-background" variant="outline">
+			{pending && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+			Sign out
+		</Button>
 	);
 }
