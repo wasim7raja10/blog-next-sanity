7abe23bf1090138239181a2371b79ec8f90a5850
Wasim Raja <wasim7raja10@gmail.com>
2024-03-06 23:22:35 +0530

    added icon in auth button

--- components/login-button.js
+++ components/login-button.js
@@ -3,15 +3,17 @@
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { Button } from "./ui/button";
 import AuthModal from "./auth-modal";
+import { LogIn } from "lucide-react";
 
 export default function LoginButton() {
 	return (
 		<Dialog>
 			<DialogTrigger asChild>
 				<Button
-					className="bg-Dialog hover:bg-background px-3 sm:px-8 h-8 sm:h-10"
+					className="bg-Dialog hover:bg-background px-3 sm:px-4 h-8 sm:h-10"
 					variant="outline"
 				>
+					<LogIn className="mr-2 h-4 w-4" />
 					Login
 				</Button>
 			</DialogTrigger>
