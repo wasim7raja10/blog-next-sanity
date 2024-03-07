e5f2a8d1f1eb00658bfc11e683688ca234fd91dc
Wasim Raja <wasim7raja10@gmail.com>
2024-03-07 23:26:25 +0530

    clipboard button done

--- components/clipboard-button.js
+++ components/clipboard-button.js
@@ -1,15 +1,33 @@
 "use client";
 
-import { Clipboard } from "lucide-react";
+import { Clipboard, ClipboardCheck } from "lucide-react";
 import { Button } from "./ui/button";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
+import { useState } from "react";
+import { useToast } from "./ui/use-toast";
 
 export default function ClipboardButton() {
+	const [copied, setCopied] = useState(false);
+	const { toast } = useToast();
+
+	function copy() {
+		navigator.clipboard.writeText(window.location.href);
+		setCopied(true);
+		toast({
+			title: "Link copied.",
+		});
+
+		// Reset copied state to false after 3 seconds
+		setTimeout(() => {
+			setCopied(false);
+		}, 5000);
+	}
+
 	return (
 		<Tooltip>
 			<TooltipTrigger asChild>
-				<Button variant="ghost" size="icon">
-					<Clipboard />
+				<Button variant="ghost" size="icon" onClick={copy}>
+					{copied ? <ClipboardCheck /> : <Clipboard />}
 				</Button>
 			</TooltipTrigger>
 			<TooltipContent>
