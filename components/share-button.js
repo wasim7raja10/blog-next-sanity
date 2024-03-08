5bd40a70bfd5e1bd08e20dd5df900d5163bd11f5
Wasim Raja <wasim7raja10@gmail.com>
2024-03-08 21:25:23 +0530

    dropdown menu done

--- components/share-button.js
+++ components/share-button.js
@@ -4,17 +4,36 @@ import { Share2 } from "lucide-react";
 import { Button } from "./ui/button";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+
 export default function ShareButton() {
 	return (
-		<Tooltip>
-			<TooltipTrigger asChild>
-				<Button variant="ghost" size="icon">
-					<Share2 />
-				</Button>
-			</TooltipTrigger>
-			<TooltipContent>
-				<p>Share</p>
-			</TooltipContent>
-		</Tooltip>
+		<DropdownMenu>
+			<Tooltip>
+				<TooltipTrigger asChild>
+					<DropdownMenuTrigger asChild>
+						<Button variant="ghost" size="icon">
+							<Share2 />
+						</Button>
+					</DropdownMenuTrigger>
+				</TooltipTrigger>
+				<TooltipContent>
+					<p>Share</p>
+				</TooltipContent>
+			</Tooltip>
+			<DropdownMenuContent align="end" className="w-[160px]">
+				<DropdownMenuItem>Share to Twitter</DropdownMenuItem>
+				<DropdownMenuItem>Share to Linkedin</DropdownMenuItem>
+				<DropdownMenuItem>Share to Reddit</DropdownMenuItem>
+				<DropdownMenuItem>Share to Facebook</DropdownMenuItem>
+				<DropdownMenuItem>Share to Mastodon</DropdownMenuItem>
+				<DropdownMenuItem>Share Post via...</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
 	);
 }
