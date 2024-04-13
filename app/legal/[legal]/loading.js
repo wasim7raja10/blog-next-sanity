fad78aab87bbeca09de3e633573c9fbb207ab1c7
Wasim Raja <wasim7raja10@gmail.com>
2024-04-14 00:15:18 +0530

    legal loading done

--- app/legal/[legal]/loading.js
+++ app/legal/[legal]/loading.js
@@ -2,7 +2,7 @@ import LoadingIcon from "@/components/loading-icon";
 
 export default function Loading() {
 	return (
-		<div className="bg-background fixed top-0 bottom-0 left-0 right-0 w-full h-screen grid place-content-center">
+		<div className="bg-background w-full h-[80vh] grid place-content-center">
 			<LoadingIcon />
 		</div>
 	);
