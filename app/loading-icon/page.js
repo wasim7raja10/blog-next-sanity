d3a218970df71b1ff205dd203231ebe62ee0d752
Wasim Raja <wasim7raja10@gmail.com>
2024-04-13 23:10:57 +0530

    done

---
+++ app/loading-icon/page.js
@@ -0,0 +1,9 @@
+import LoadingIcon from "@/components/loading-icon";
+
+export default function Page() {
+	return (
+		<div className="w-full h-full grid place-content-center">
+			<LoadingIcon />
+		</div>
+	);
+}
