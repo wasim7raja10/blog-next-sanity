bdb27bf1399b2fb096a9dfa16d0db26c6d0a0174
Wasim Raja <wasim7raja10@gmail.com>
2024-04-14 12:40:15 +0530

    heading design change

--- components/post-title.js
+++ components/post-title.js
@@ -1,6 +1,6 @@
 export default function PostTitle({ children }) {
   return (
-    <h1 className="font-medium text-3xl sm:text-5xl sm:leading-[64px] leading-10">
+    <h1 className="font-medium text-2xl sm:text-4xl sm:leading-[54px] leading-9">
       {children}
     </h1>
   )
